6883a8869e11b4f181d70f2ae4d6c3dba2579433
unknown <bharathpg1997@gmail.com>
2021-06-02 10:58:40 +0530

    Added Loggers

--- src/main/java/com/example/Springboot/controller/DepartmentController.java
+++ src/main/java/com/example/Springboot/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.example.Springboot.controller;
 
 import com.example.Springboot.entity.Department;
 import com.example.Springboot.service.DepartmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +16,17 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside saveDepartment of DepartmentController");
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping("/departments")
     public List<Department> fetchDepartmentList() {
+        LOGGER.info("Inside fetchDepartmentList of DepartmentController");
         return departmentService.fetchDepartmentList();
     }
 
